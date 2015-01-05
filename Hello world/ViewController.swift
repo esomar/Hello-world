d82d4d5e5d5accce1891a24f82e3ad44bedebe4a
Essam Eliwa <Essam@Essams-Mac-mini.local>
2015-01-05 12:10:28 +0200

    initial local commit

---
+++ Hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by Essam Eliwa on 1/4/15.
+//  Copyright (c) 2015 Essam Eliwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
